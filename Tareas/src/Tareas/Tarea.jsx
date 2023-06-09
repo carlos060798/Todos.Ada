c47d9bf0b5ba5d2da0c77d36cdaf5948c8420806
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-06-09 10:14:42 -0500

    version  final

--- Tareas/src/Tareas/Tarea.jsx
+++ Tareas/src/Tareas/Tarea.jsx
@@ -1,48 +1,69 @@
-import React, { useRef } from "react";
-
+import React, { useRef, useState,useEffect } from "react";
 import "./tarea.css";
 
-function TareaItem({tarea, toggleTodo,deleteTarea,editarTarea}) {
-  const { id, task, completed } = tarea; 
-  const liItem = useRef();  
+function TareaItem({ tarea, toggleTodo, deleteTarea, editarTarea }) {
+  const { id, task, completed } = tarea;
+  const liItem = useRef();
+  const [isChecked, setIsChecked] = useState(completed);
+ 
+  useEffect(() => {
+    setIsChecked(completed);
+  }, [completed]); 
+  
   const handleTodoClick = () => {
     toggleTodo(id);
+    setIsChecked(!isChecked);
   };
+
   const handleCheckboxClick = (event) => {
     const checkbox = event.target;
     const elemento = checkbox.parentNode;
 
-    if (checkbox.checked) {
-      elemento.classList.add("text-decoration-line-through");
-    } else {
-      elemento.classList.remove("text-decoration-line-through");
-    }
-  }; 
+    if (!checkbox.checked) {
+      setCompletedStyle("text-decoration-line-through");
+    } 
+  };
 
-  const hadleTododeleteid=()=>{
-    deleteTarea(id)
-  }
+  const hadleTododeleteid = () => {
+    deleteTarea(id);
+  };
 
-  const hadleTodoEdit=()=>{
-    editarTarea(id)
-  }
+  const hadleTodoEdit = () => {
+    editarTarea(id);
+  };
+
   return (
-    <li className="list-group-item d-flex  justify-content-between mt-3"> 
-     <p className="h5">
-      <input
-        type="checkbox"
-        className="mx-2 btn-checkbox"
-        checked={completed}
-        onClick={handleCheckboxClick}
-        onChange={handleTodoClick}
-      />
-      {task}{" "} </p>
-       <div>
-      <button className="btn  mx-1 text-danger btn-list"  onClick={hadleTododeleteid}><i className="bi bi-x-circle"></i></button>
-      <button className="btn  text-success btn-list"  onClick={hadleTodoEdit}><i className="bi bi-pencil-square"></i></button>
+    <li  className={`list-group-item d-flex justify-content-between mt-3 ${
+      isChecked ? "text-decoration-line-through" : ""
+    }`}>
+      <p className="h5">
+        <input
+          type="checkbox"
+          className="mx-2 btn-checkbox"
+          checked={completed}
+          onClick={handleCheckboxClick}
+          onChange={handleTodoClick}
+        />
+        {task}{" "}
+      </p>
+      <div>
+        <button
+          className="btn mx-1 text-danger btn-list"
+          onClick={hadleTododeleteid}
+        >
+          <i className="bi bi-x-circle"></i>
+        </button>
+        <button
+          className="btn text-success btn-list"
+          onClick={hadleTodoEdit}
+        >
+          <i className="bi bi-pencil-square"></i>
+        </button>
       </div>
     </li>
   );
 }
 
 export default TareaItem;
+
+
