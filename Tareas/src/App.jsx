35b9707de7973e193a817ab4c870d6e325904e4c
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-06-25 20:46:16 -0500

    mejora resposiba

--- Tareas/src/App.jsx
+++ Tareas/src/App.jsx
@@ -1,4 +1,91 @@
-import { Box, Flex, Spacer, Breadcrumb, BreadcrumbItem, HStack } from "@chakra-ui/react";
+
+import React from 'react';
+import { BrowserRouter as Router, Link as RouterLink, Routes, Route } from 'react-router-dom';
+import { Box, Flex, Spacer, Breadcrumb, BreadcrumbItem, VStack, IconButton, useDisclosure, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, HStack } from '@chakra-ui/react';
+import { CopyIcon, ChevronRightIcon, HamburgerIcon } from '@chakra-ui/icons';
+import ListTask from "./ListTasks";
+import Home from "./paginas-inicio/Home";
+import About from "./paginas-inicio/About";
+function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Router>
+      <Box className="container" color="blue.500">
+        <Flex alignItems="center" justifyContent="space-between" p={4}>
+          <HStack spacing={2}>
+            <CopyIcon w={6} h={15} boxSize={8} color="blue.500" />
+            <Box fontSize="xl" fontWeight="bold">
+              Task Manager
+            </Box>
+          </HStack>
+
+          <Spacer />
+          <HStack display={{ base: 'none', md: 'flex' }} spacing="8px">
+            <Breadcrumb separator={<ChevronRightIcon color="gray.500" />}>
+              <BreadcrumbItem>
+                <RouterLink to="/" mr={4} fontSize="lg" fontWeight="600">
+                  Home
+                </RouterLink>
+              </BreadcrumbItem>
+
+              <BreadcrumbItem>
+                <RouterLink to="/about" mr={4} fontSize="lg">
+                  About
+                </RouterLink>
+              </BreadcrumbItem>
+              <BreadcrumbItem>
+                <RouterLink to="/listtask" mr={4} fontSize="lg">
+                  List Task
+                </RouterLink>
+              </BreadcrumbItem>
+            </Breadcrumb>
+          </HStack>
+          <IconButton
+            display={{ base: 'flex', md: 'none' }}
+            icon={<HamburgerIcon />}
+            variant="ghost"
+            aria-label="Menu"
+            onClick={onOpen}
+          />
+        </Flex>
+
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/listtask" element={<ListTask />} />
+        </Routes>
+      </Box>
+
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Menu</DrawerHeader>
+            <DrawerBody>
+              <VStack spacing={4}>
+                <RouterLink to="/" display="block" fontSize="lg" my={2}>
+                  Home
+                </RouterLink>
+                <RouterLink to="/about" display="block" fontSize="lg" my={2}>
+                  About
+                </RouterLink>
+                <RouterLink to="/listtask" display="block" fontSize="lg" my={2}>
+                  List Task
+                </RouterLink>
+              </VStack>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </Router>
+  );
+}
+
+export default App;
+
+
+/*import { Box, Flex, Spacer, Breadcrumb, BreadcrumbItem, HStack } from "@chakra-ui/react";
 import { CopyIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import { BrowserRouter as Router, Routes, Route, Link as RouterLink } from "react-router-dom";
 import ListTask from "./ListTasks";
@@ -7,7 +94,8 @@ import About from "./paginas-inicio/About";
 
 function  App(){
   return (
-    <Router>
+
+   { /* <Router>
       <Box className="container" color="blue.500" >
         <Flex alignItems="center" justifyContent="space-between" p={4} >
           <HStack spacing={2}>
@@ -44,7 +132,7 @@ function  App(){
           <Route path="/listtask" element={<ListTask />} />
         </Routes>
       </Box>
-    </Router>
+   </Router>
   );
 };
 
@@ -53,7 +141,7 @@ export default App;
 
 
 
-
+*/
 
 
 /*import { Box, Button,Stack,Text } from "@chakra-ui/react";
--- Tareas/src/paginas-inicio/About.jsx
+++ Tareas/src/paginas-inicio/About.jsx
@@ -1,59 +1,44 @@
-import {
-  Box,
-  Button,
-  Heading,
-  Text,
-  UnorderedList,
-  ListItem,
-} from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-
+import React from 'react';
+import { Box, Button, Heading, Text, UnorderedList, ListItem, Stack, Divider } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
     <>
-      <Box backgroundColor={"blue.500"} color="white" textAlign="center" py={5}>
+      <Box backgroundColor="blue.500" color="white" textAlign="center" py={5}>
         <Heading as="h1" fontSize="4xl">
           Task Manager
         </Heading>
         <Text fontSize="xl" mt={4}>
           Stay organized and productive
         </Text>
-       <Link to="/" mt={4}>
-        <Button colorScheme="blackAlpha" mt={4}>
-          Back to Home
-        </Button>
+        <Link to="/" mt={4}>
+          <Button colorScheme="blackAlpha" mt={4}>
+            Back to Home
+          </Button>
         </Link>
       </Box>
-      <Box
-        mt={5}
-        p={6}
-        maxW="container"
-        display="flex"
-        flexDirection="row"
-        gap={8}
-      >
-        <Box flexBasis="60%"> 
+
+      <Stack direction={{ base: 'column', md: 'row' }} mt={5} p={6} maxW="container" spacing={8}>
+        <Box flexBasis={{ base: '100%', md: '60%' }}>
           <Heading as="h2" fontSize="2xl">
             About Task Manager
           </Heading>
           <Text mt={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at
-            ligula ut turpis auctor scelerisque. Nulla facilisi. In eget arcu id
-            ligula posuere bibendum. Aliquam ut vulputate lacus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at ligula ut turpis auctor scelerisque. Nulla
+            facilisi. In eget arcu id ligula posuere bibendum. Aliquam ut vulputate lacus.
           </Text>
           <Text mt={4}>
-            Phasellus quis lectus in massa fermentum pharetra. In nec arcu eu
-            felis consectetur fringilla. Duis eu massa consectetur, viverra
-            ipsum vitae, dapibus lorem. Donec pharetra rhoncus metus non
-            vestibulum.
+            Phasellus quis lectus in massa fermentum pharetra. In nec arcu eu felis consectetur fringilla. Duis eu massa
+            consectetur, viverra ipsum vitae, dapibus lorem. Donec pharetra rhoncus metus non vestibulum.
           </Text>
         </Box>
 
-        <Box flexBasis="40%">
+        <Box flexBasis={{ base: '100%', md: '40%' }}>
           <Heading as="h2" fontSize="2xl">
             Key Features
           </Heading>
+          <Divider mt={4} />
           <UnorderedList mt={4}>
             <ListItem>Task creation and management</ListItem>
             <ListItem>Priority and due date settings</ListItem>
@@ -62,13 +47,16 @@ function About() {
             <ListItem>Progress tracking and reporting</ListItem>
           </UnorderedList>
         </Box>
-      </Box>
+      </Stack>
 
-      <Box color={"blue.500"} textAlign="center" py={3}>
-        <Text fontWeight="semibold"  fontSize="2xl">Task Manager &copy; 2023</Text>
+      <Box color="blue.500" textAlign="center" py={3}>
+        <Text fontWeight="semibold" fontSize="2xl">
+          Task Manager © 2023
+        </Text>
       </Box>
     </>
   );
 }
 
 export default About;
+
