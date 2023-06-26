44c05677b0536ca036d64b7c159b7e1df8b1bbe8
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-06-26 10:27:11 -0500

    version final

--- Tareas/src/Tareas/Form.jsx
+++ Tareas/src/Tareas/Form.jsx
@@ -1,4 +1,56 @@
-import { Box, Text, Stack, Input, Textarea, Flex, Button } from "@chakra-ui/react";
+import { Box, Text, Stack, Input, Textarea, Flex, Button } from '@chakra-ui/react';
+
+function FormTodo({ handleTareaADD, Tarearef, descriptionRef, tareaEror, descriptionError }) {
+  return (
+    <Box w={500} p={6}>
+      <Stack spacing={4} mx={8} borderWidth="3px" borderColor="blue.300" borderRadius="md" p={6}>
+        <Text textAlign="center" color="blue.500" fontWeight="bold" fontSize="xl" mt={4}>
+          Create Todo List
+        </Text>
+        <Box>
+          <Text textAlign="center" fontSize="lg" fontWeight="bold">
+            Title
+          </Text>
+          <Input
+            ref={Tarearef}
+            variant="filled"
+            color="black"
+            placeholder="Enter a title for your todo"
+          />
+          {tareaEror && <Box color="red.500">{tareaEror}</Box>}
+        </Box>
+        <Box>
+          <Text textAlign="center" fontWeight="bold">
+            Task Description
+          </Text>
+          <Textarea
+            ref={descriptionRef}
+            variant="filled"
+            color="black"
+            placeholder="Enter a description for your todo"
+            resize="vertical"
+            minHeight="120px"
+          />
+          {descriptionError && <Box color="red.500">{descriptionError}</Box>}
+        </Box>
+        <Flex justifyContent="center" mt={4}>
+          <Button colorScheme="green" onClick={handleTareaADD}>
+          <i className="bi bi-plus-circle"></i>
+          </Button>
+        </Flex>
+      </Stack>
+    </Box>
+  );
+}
+
+export default FormTodo;
+
+
+
+
+
+
+/*import { Box, Text, Stack, Input, Textarea, Flex, Button } from "@chakra-ui/react";
 
 
 function FormTodo({ handleTareaADD,Tarearef,descriptionRef,tareaEror,descriptionError}) {
@@ -41,3 +93,4 @@ function FormTodo({ handleTareaADD,Tarearef,descriptionRef,tareaEror,description
   )}
 
 export default FormTodo;
+*/
